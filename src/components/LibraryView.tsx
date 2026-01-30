273759e592577d89cdb1a31eca02c022b87d9186
Hardy Medry Dieu-Veil NKODIA <93324399+HardyNk242@users.noreply.github.com>
2026-01-30 11:46:04 +0900

    Update LibraryView.tsx

--- src/components/LibraryView.tsx
+++ src/components/LibraryView.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { SCIMAGO_DOMAINS } from '../constants'; // Garder cet import
-import { THESES_LIBRARY } from '../data/library'; // <--- NOUVEL IMPORT ICI
+import { THESES_LIBRARY, SCIMAGO_DOMAINS } from '../data/library';
 import { Thesis } from '../types';
 import SubmitPublicationModal from './SubmitPublicationModal';
 
